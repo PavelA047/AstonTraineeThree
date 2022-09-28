80f4da5cad6c3b7418c8528d47c5e517ef3f2b65
pavel <zzzzfogzzzz@gmail.com>
2022-09-29 05:08:00 +0600

    commit2

--- src/main/java/org/example/BootstrapListener.java
+++ src/main/java/org/example/BootstrapListener.java
@@ -1,20 +1,36 @@
 package org.example;
 
-import org.example.persist.User;
 import org.example.persist.UserRepository;
+import org.example.persist.UserRepositoryDb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 @WebListener
 public class BootstrapListener implements ServletContextListener {
+    private static final Logger logger = LoggerFactory.getLogger(BootstrapListener.class);
+    private static DataSource dataSource;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Context ctx = new InitialContext();
+            dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/users");
+        } catch (ClassNotFoundException | NamingException e) {
+            logger.error("", e);
+        }
+    }
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        UserRepository userRepository = new UserRepository();
-        userRepository.insert(new User("Alex"));
-        userRepository.insert(new User("Petr"));
-        userRepository.insert(new User("Filip"));
+        UserRepository userRepository = new UserRepositoryDb(dataSource);
         sce.getServletContext().setAttribute("userRepository", userRepository);
     }
 }
--- src/main/java/org/example/persist/User.java
+++ src/main/java/org/example/persist/User.java
@@ -1,13 +1,35 @@
 package org.example.persist;
 
+import java.util.Set;
+
 public class User {
 
     private Long id;
 
     private String username;
 
+    private Set<String> role;
+
     public User(String username) {
         this.username = username;
+    }
+
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public User(String username, Set<String> role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
     }
 
     public Long getId() {
